d83349da8c45e5f78b7578afe63d388dc10026b0
Jingi Kim <copyx00@gmail.com>
2021-07-10 18:05:01 +0900

    Practice optional property

--- object.ts
+++ object.ts
@@ -3,3 +3,9 @@ const person1 = { name: "jake", age: 12 };
 // person1은 'object' 타입이 아닌 '{ name: string, age: number }'형식의 object 타입
 
 const person2 = Object.create({ name: "jake", age: 12 });
+
+function optionalProps(obj: { a: string; b?: string }) {}
+
+// 둘 다 괜춘쓰
+optionalProps({ a: "aa" });
+optionalProps({ a: "aa", b: "bb" });
